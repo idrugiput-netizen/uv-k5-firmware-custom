389bb6b8379e17322b36b4e23b5b50605b45741e
idrugiput-netizen <idrugiput@gmail.com>
2025-11-09 20:22:08 +0000

    Patch EEPROM za M24M02 (256 KB) sa 32-bit adresama

--- App/driver/eeprom.c
+++ App/driver/eeprom.c
@@ -1,19 +1,3 @@
-/* Copyright 2023 Dual Tachyon
- * https://github.com/DualTachyon
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- */
-
 #include <stddef.h>
 #include <string.h>
 
@@ -21,43 +5,42 @@
 #include "driver/i2c.h"
 #include "driver/system.h"
 
-void EEPROM_ReadBuffer(uint16_t Address, void *pBuffer, uint8_t Size)
+void EEPROM_ReadBuffer(uint32_t Address, void *pBuffer, uint16_t Size)
 {
-    I2C_Start();
-
-    I2C_Write(0xA0);
-
-    I2C_Write((Address >> 8) & 0xFF);
-    I2C_Write((Address >> 0) & 0xFF);
+    if (pBuffer == NULL || Address + Size > EEPROM_MAX_ADDRESS)
+        return;
 
     I2C_Start();
+    I2C_Write(0xA0);  // Write mode za adresu
 
-    I2C_Write(0xA1);
+    // 16-bitna adresa za M24M02
+    I2C_Write((Address >> 8) & 0xFF);  // MSB
+    I2C_Write(Address & 0xFF);         // LSB
 
+    I2C_Start();
+    I2C_Write(0xA1);  // Read mode
     I2C_ReadBuffer(pBuffer, Size);
-
     I2C_Stop();
 }
 
-void EEPROM_WriteBuffer(uint16_t Address, const void *pBuffer)
+void EEPROM_WriteBuffer(uint32_t Address, const void *pBuffer, uint16_t Size)
 {
-    if (pBuffer == NULL || Address >= 0x2000)
+    if (pBuffer == NULL || Address + Size > EEPROM_MAX_ADDRESS)
         return;
 
-
-    uint8_t buffer[8];
-    EEPROM_ReadBuffer(Address, buffer, 8);
-    if (memcmp(pBuffer, buffer, 8) == 0) {
+    uint8_t buffer[32];  // uporedi sa postojećim podacima, možeš povećati veličinu ako želiš
+    EEPROM_ReadBuffer(Address, buffer, Size);
+    if (memcmp(pBuffer, buffer, Size) == 0)
         return;
-    }
 
     I2C_Start();
-    I2C_Write(0xA0);
-    I2C_Write((Address >> 8) & 0xFF);
-    I2C_Write((Address >> 0) & 0xFF);
-    I2C_WriteBuffer(pBuffer, 8);
+    I2C_Write(0xA0);  // Write mode
+
+    I2C_Write((Address >> 8) & 0xFF);  // MSB
+    I2C_Write(Address & 0xFF);         // LSB
+
+    I2C_WriteBuffer(pBuffer, Size);
     I2C_Stop();
 
-    // give the EEPROM time to burn the data in (apparently takes 5ms)
-    SYSTEM_DelayMs(8);
+    SYSTEM_DelayMs(8);  // čekanje dok EEPROM završi upis
 }
--- App/driver/eeprom.h
+++ App/driver/eeprom.h
@@ -2,16 +2,6 @@
  * https://github.com/DualTachyon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
  */
 
 #ifndef DRIVER_EEPROM_H
@@ -19,8 +9,10 @@
 
 #include <stdint.h>
 
-void EEPROM_ReadBuffer(uint16_t Address, void *pBuffer, uint8_t Size);
-void EEPROM_WriteBuffer(uint16_t Address, const void *pBuffer);
+#define EEPROM_MAX_ADDRESS 0x40000  // 256 KB
+
+// Funkcije za čitanje/pisanje EEPROM-a
+void EEPROM_ReadBuffer(uint32_t Address, void *pBuffer, uint16_t Size);
+void EEPROM_WriteBuffer(uint32_t Address, const void *pBuffer, uint16_t Size);
 
 #endif
-
